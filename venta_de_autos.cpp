2eca3b2a9c1ad33e3ab15858ab0e29fcc8a7994b
JmiguelRamriez <A01712628@tec.mx>
2024-11-14 10:38:01 -0600

    venta_de_autos.cpp

--- venta_de_autos.cpp
+++ venta_de_autos.cpp
@@ -6,17 +6,17 @@ using namespace std;
 
 
 int main(){
-    Coche coche1("Toyota", "Corolla", 2023, "Automática", 22000.0, 4, true);
-    Coche coche2("Ford", "Explorer", 2022, "Automática", 35000.0, 5, true);
+    Coche coche1("Toyota", "Corolla", 2023, "Automatica", 22000.0, 4, true);
+    Coche coche2("Ford", "Explorer", 2022, "Automatica", 35000.0, 5, true);
     Coche coche3("Chevrolet", "Camaro", 2021, "Manual", 40000.0, 2, true);
-    Coche coche4("Tesla", "Model 3", 2023, "Automática", 45000.0, 4, true);
+    Coche coche4("Tesla", "Model 3", 2023, "Automatica", 45000.0, 4, true);
     Coche coche5("Nissan", "Versa", 2020, "Manual", 16000.0, 4, false);
-    Coche coche6("RAM", "1500", 2023, "Automática", 42000.0, 4, true);
+    Coche coche6("RAM", "1500", 2023, "Automatica", 42000.0, 4, true);
 
     Moto moto1("Yamaha", "YZF-R6", 2021, "Manual", 12000.0, "Deportiva");
     Moto moto2("Harley-Davidson", "Iron 883", 2023, "Manual", 10000.0, "Cruiser");
     Moto moto3("BMW", "R 1250 GS", 2022, "Manual", 18000.0, "Aventura");
-    Moto moto4("Honda", "PCX 150", 2020, "Automática", 3500.0, "Scooter");
+    Moto moto4("Honda", "PCX 150", 2020, "Automatica", 3500.0, "Scooter");
 
     Consecionaria consecionaria1("al sur de la ciudad");
     Consecionaria consecionaria2("al norte de la ciudad"); 
@@ -24,13 +24,13 @@ int main(){
     consecionaria1.agregarVehiculo(coche1); 
     consecionaria1.agregarVehiculo(coche1);
     consecionaria1.agregarVehiculo(coche2);
-    consecionaria1.agregarMoto(moto1);
-    consecionaria1.agregarMoto(moto2);
+    consecionaria1.agregarVehiculo(moto1);
+    consecionaria1.agregarVehiculo(moto2);
     consecionaria2.agregarVehiculo(coche3);
     consecionaria2.agregarVehiculo(coche4);
     consecionaria2.agregarVehiculo(coche5);
-    consecionaria2.agregarMoto(moto3);
-    consecionaria2.agregarMoto(moto4);
+    consecionaria2.agregarVehiculo(moto3);
+    consecionaria2.agregarVehiculo(moto4);
     
     consecionaria1.get_ubicacion();
     consecionaria2.get_ubicacion();
