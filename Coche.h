c3ff901982fe33ca5eacd1060bf30ba12fe7bc31
JmiguelRamriez <A01712628@tec.mx>
2024-11-24 19:20:02 -0600

    Coche.h

--- Coche.h
+++ Coche.h
@@ -1,39 +1,55 @@
-#include<iostream>
-#include<string>
-#include "Vehiculo.h"
-using namespace std;
+/*
+ * Clase Coche
+ * Hereda de Vehiculo y contiene atributos y métodos específicos para coches.
+ */
 
-class Coche: public Vehiculo{
+#ifndef COCHE_H
+#define COCHE_H
+
+#include "Vehiculo.h"
+#include <string>
+
+// Clase Coche que hereda de Vehiculo
+class Coche: public Vehiculo {
     private:
-        int numero_puerta;
-        bool airbag;
-    
+        // Atributos específicos de Coche
+        int numero_puerta;  // Número de puertas
+        bool airbag;        // Si el coche tiene airbag
+
     public:
-        Coche();
-        Coche(string m, string mod, int y, string t, float p, int n_m, bool a)
-        : Vehiculo(m, mod, y, t, p), numero_puerta(n_m), airbag(a){};
-        
-        int get_numero_puerta();
-        bool get_airbag();
+        // Métodos públicos de la clase Coche
+        Coche();  // Constructor por defecto
+        Coche(string m, string mod, int y, string t, float p, int n_m, bool a); // Constructor parametrizado
+
+        // Getters para los atributos específicos del coche
+        int get_numero_puerta();  
+        bool get_airbag();  
+
+        // Método para obtener la información detallada del coche
         string informacion();
 
+        // Setters para los atributos específicos del coche
         void set_numero_puerta(int);
         void set_airbag(bool);
 };
 
-Coche::Coche(){
+// Constructor por defecto
+Coche::Coche() {
     numero_puerta = 0;
     airbag = false;
 }
 
-int Coche::get_numero_puerta(){
-    return numero_puerta;
-}
+// Constructor parametrizado
+Coche::Coche(string m, string mod, int y, string t, float p, int n_m, bool a)
+    : Vehiculo(m, mod, y, t, p), numero_puerta(n_m), airbag(a) {}
 
-bool Coche::get_airbag(){
-    return airbag;
-}
+// Getter para el número de puertas
+int Coche::get_numero_puerta() { return numero_puerta; }
 
+// Getter para el airbag
+bool Coche::get_airbag() { return airbag; }
+
+// Método para obtener la información detallada del coche
 string Coche::informacion() {
     string info = Vehiculo::informacion();
     info += "Numero de puertas: " + to_string(numero_puerta) + "\n";
@@ -41,11 +57,10 @@ string Coche::informacion() {
     return info;
 }
 
+// Setter para el número de puertas
+void Coche::set_numero_puerta(int n) { numero_puerta = n; }
 
-void Coche::set_numero_puerta(int n_m){
-    numero_puerta = n_m;
-}
+// Setter para el airbag
+void Coche::set_airbag(bool a) { airbag = a; }
 
-void Coche::set_airbag(bool a){
-    airbag = a;
-}
+#endif
