4ae1ef5623bfefabfc7c3f34dfefa6f9760b85dc
JmiguelRamriez <A01712628@tec.mx>
2024-11-24 19:19:11 -0600

    Cliente.h

--- Cliente.h
+++ Cliente.h
@@ -1,63 +1,63 @@
-#include <string>
-using namespace std;
+/*
+ * Clase Cliente
+ * Contiene los métodos y atributos básicos para manejar la información de un cliente
+ * que puede estar interesado en vehículos de distintas concesionarias.
+ */
 
-class Cliente{
+#ifndef CLIENTE_H
+#define CLIENTE_H
+
+#include <string>
+
+// Clase Cliente
+class Cliente {
     private:
-        string nombre;
-        string correo;
-        int numero;
-        string consecionaria;
+        // Atributos privados del cliente
+        string nombre;        // Nombre del cliente
+        string correo;        // Correo electrónico del cliente
+        int numero;           // Número telefónico del cliente
+        string consecionaria; // Nombre de la concesionaria elegida
 
     public:
-        Cliente();
-        Cliente(string n, string c, int num, string con ): nombre (n), correo (c), numero (num), consecionaria(con){};
-        
+        // Métodos públicos de la clase Cliente
+        Cliente();  // Constructor por defecto
+        Cliente(string n, string c, int num, string con); // Constructor parametrizado
+
+        // Métodos getter
         string get_nombre();
         string get_correo();
         int get_numero();
         string get_concesionaria();
 
+        // Métodos setter
         void set_nombre(string);
         void set_correo(string);
         void set_numero(int);
         void set_consecionaria(string);
 };
 
-Cliente::Cliente(){
+// Constructor por defecto
+Cliente::Cliente() {
     nombre = "";
     correo = "";
     numero = 0;
     consecionaria = "";
 }
 
-string Cliente::get_nombre(){
-    return nombre;
-}
+// Constructor parametrizado
+Cliente::Cliente(string n, string c, int num, string con)
+    : nombre(n), correo(c), numero(num), consecionaria(con) {}
 
-string Cliente::get_correo(){
-    return correo;
-}
+// Métodos getter
+string Cliente::get_nombre() { return nombre; }
+string Cliente::get_correo() { return correo; }
+int Cliente::get_numero() { return numero; }
+string Cliente::get_concesionaria() { return consecionaria; }
 
-int Cliente::get_numero(){
-    return numero;
-}
+// Métodos setter
+void Cliente::set_nombre(string n) { nombre = n; }
+void Cliente::set_correo(string c) { correo = c; }
+void Cliente::set_numero(int num) { numero = num; }
+void Cliente::set_consecionaria(string con) { consecionaria = con; }
 
-string Cliente::get_concesionaria(){
-    return consecionaria;
-}
-
-void Cliente::set_nombre(string n){
-    nombre = n;
-}
-
-void Cliente::set_correo(string c){
-    correo = c;
-}
-
-void Cliente::set_numero(int num){
-    numero = num;
-}
-
-void Cliente::set_consecionaria(string con) {
-    consecionaria = con;
-}
+#endif
