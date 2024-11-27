72a77b9b1f7b0af9c2ad4a5eb50a3a822d8ae39a
JmiguelRamriez <A01712628@tec.mx>
2024-11-26 20:12:58 -0600

    Cliente.h

--- Cliente.h
+++ Cliente.h
@@ -15,24 +15,24 @@ class Cliente {
         // Atributos privados del cliente
         string nombre;        // Nombre del cliente
         string correo;        // Correo electrónico del cliente
-        char numero;           // Número telefónico del cliente
+        string numero;           // Número telefónico del cliente
         string consecionaria; // Nombre de la concesionaria elegida
 
     public:
         // Métodos públicos de la clase Cliente
         Cliente();  // Constructor por defecto
-        Cliente(string n, string c, int num, string con); // Constructor parametrizado
+        Cliente(string n, string c, string num, string con); // Constructor parametrizado
 
         // Métodos getter
         string get_nombre();
         string get_correo();
-        char get_numero();
+        string get_numero();
         string get_concesionaria();
 
         // Métodos setter
         void set_nombre(string);
         void set_correo(string);
-        void set_numero(char);
+        void set_numero(string);
         void set_consecionaria(string);
 };
 
@@ -40,24 +40,24 @@ class Cliente {
 Cliente::Cliente() {
     nombre = "";
     correo = "";
-    numero = 0;
+    numero = "";
     consecionaria = "";
 }
 
 // Constructor parametrizado
-Cliente::Cliente(string n, string c, int num, string con)
+Cliente::Cliente(string n, string c, string num, string con)
     : nombre(n), correo(c), numero(num), consecionaria(con) {}
 
 // Métodos getter
 string Cliente::get_nombre() { return nombre; }
 string Cliente::get_correo() { return correo; }
-char Cliente::get_numero() { return numero; }
+string Cliente::get_numero() { return numero; }
 string Cliente::get_concesionaria() { return consecionaria; }
 
 // Métodos setter
 void Cliente::set_nombre(string n) { nombre = n; }
 void Cliente::set_correo(string c) { correo = c; }
-void Cliente::set_numero(char num) { numero = num; }
+void Cliente::set_numero(string num) { numero = num; }
 void Cliente::set_consecionaria(string con) { consecionaria = con; }
 
 #endif
