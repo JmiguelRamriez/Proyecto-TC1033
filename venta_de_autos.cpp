9d8b212e2b7376f6cae711dec7dafd44c44f965b
JmiguelRamriez <A01712628@tec.mx>
2024-11-28 19:56:10 -0600

    venta_de_autos.cpp

--- venta_de_autos.cpp
+++ venta_de_autos.cpp
@@ -14,21 +14,33 @@
  */
 
 #include<iostream>    // Biblioteca estándar para operaciones de entrada y salida
-#include "Vehiculo.h" // Biblioteca que incluye la clase Vehiculo, la cual sirve de base para los vehículos como Coche y Moto
-#include "Consecionaria.h"  // Biblioteca que contiene la clase Consecionaria, que gestiona los vehículos disponibles en la concesionaria
+#include "Vehiculo.h" // Biblioteca que incluye la clase Vehiculo
+#include "Consecionaria.h"  // Biblioteca que contiene la clase Consecionaria, que gestiona los vehículos 
 #include "Cliente.h"  // Biblioteca que contiene la clase Cliente, que maneja los datos del cliente
+#include <cstdlib>
 
 using namespace std;  // Usamos el espacio de nombres estándar para simplificar el código
 
-// Función para agregar un nuevo vehículo (Coche o Moto)
+// Función para limpiar la consola dependiendo del sistema operativo
+void limpiar_consola(){
+    #ifdef _WIN32
+        system("cls");  // Comando para limpiar la consola en sistemas Windows
+    #else
+        system("clear");  // Comando para limpiar la consola en sistemas Unix/Linux
+    #endif
+}
+
+// Función para agregar un nuevo vehículo (Coche o Moto) a una concesionaria
 void agregarNuevoVehiculo(Consecionaria& concesionaria) {
     int tipoVehiculo;
+    
     cout << "Que tipo de vehiculo desea agregar?" << endl;
     cout << "1. Coche" << endl;
     cout << "2. Moto" << endl;
+    cout << "" << endl;
     cin >> tipoVehiculo;
 
-    // Crear un nuevo Coche
+    // Crear un nuevo Coche si la opción seleccionada es 1
     if (tipoVehiculo == 1) {
         string marca, modelo, transmision;
         int year;
@@ -36,11 +48,12 @@ void agregarNuevoVehiculo(Consecionaria& concesionaria) {
         int numPuertas;
         bool tieneAirbags;
 
+        // Solicitar al usuario los datos del coche
         cout << "Ingrese la marca del coche: ";
         cin >> marca;
         cout << "Ingrese el modelo del coche: ";
         cin >> modelo;
-        cout << "Ingrese el año del coche: ";
+        cout << "Ingrese el anio del coche: ";
         cin >> year;
         cout << "Ingrese el tipo de transmision (Automatica/Manual): ";
         cin >> transmision;
@@ -50,24 +63,27 @@ void agregarNuevoVehiculo(Consecionaria& concesionaria) {
         cin >> numPuertas;
         cout << "Tiene airbags? (1 para si, 0 para no): ";
         cin >> tieneAirbags;
-
-        // Crear el nuevo objeto Coche
+        
+        limpiar_consola();  // Limpiar la consola para una mejor presentación visual
+        
+        // Crear el nuevo objeto Coche con los datos proporcionados
         Coche nuevoCoche(marca, modelo, year, transmision, precio, numPuertas, tieneAirbags);
         
         // Agregar el coche a la concesionaria
         concesionaria.agregarVehiculo(nuevoCoche);
     }
-    // Crear una nueva Moto
+    // Crear una nueva Moto si la opción seleccionada es 2
     else if (tipoVehiculo == 2) {
         string marca, modelo, transmision, tipo;
         int year;
         float precio;
 
+        // Solicitar al usuario los datos de la moto
         cout << "Ingrese la marca de la moto: ";
         cin >> marca;
         cout << "Ingrese el modelo de la moto: ";
         cin >> modelo;
-        cout << "Ingrese el año de la moto: ";
+        cout << "Ingrese el anio de la moto: ";
         cin >> year;
         cout << "Ingrese el tipo de transmision (Automatica/Manual): ";
         cin >> transmision;
@@ -75,50 +91,53 @@ void agregarNuevoVehiculo(Consecionaria& concesionaria) {
         cin >> precio;
         cout << "Ingrese el tipo de moto (Deportiva, Cruiser, Aventura, etc.): ";
         cin >> tipo;
-
-        // Crear el nuevo objeto Moto
+        
+        limpiar_consola();  // Limpiar la consola para una mejor presentación visual
+        
+        // Crear el nuevo objeto Moto con los datos proporcionados
         Moto nuevaMoto(marca, modelo, year, transmision, precio, tipo);
         
         // Agregar la moto a la concesionaria
         concesionaria.agregarVehiculo(nuevaMoto);
     }
     else {
-        cout << "Opción inválida." << endl;
+        cout << "Opción invalida." << endl;  // Si la opción es inválida
     }
 }
 
-// Función para mostrar el menú de opciones para seleccionar tipo de vehículo
+// Función para mostrar el menú de opciones de vehículos (Coche o Moto)
 void menuVehiculo(){
     cout << "Que vehiculo desea agregar?" << endl;
     cout << "1. Coche" << endl;
     cout << "2. Moto" << endl;
+    cout << "" << endl; 
 }
 
-// Función para mostrar el menú de concesionarias
+// Función para mostrar el menú de concesionarias disponibles
 void mostrarMenuC(){
+    limpiar_consola();
     cout << "A cual consecionaria quiere agregar el nuevo vehiculo" << endl;
     cout << "1. Concesionaria 1" << endl;
     cout << "2. Concesionaria 2" << endl;
+    cout << "" << endl;
 }
 
 // Función para mostrar el menú principal de opciones
 void mostrarMenu(){
-    cout << "Que concesionaria quieres visitar?" << endl;
-    cout << "1. Concesionaria 1" << endl;
-    cout << "2. Concesionaria 2" << endl;
+    cout << "Que desea hacer?" << endl;
+    cout << "1. visitar concesionaria 1" << endl;
+    cout << "2. visitar concesionaria 2" << endl;
     cout << "3. Agregar vehiculo a una consecionaria" << endl;
     cout << "4. Salir" << endl; 
+    cout << "" << endl;
 }
 
 int main(){
-
     // Se crean varios objetos de la clase Coche, con diferentes atributos (marca, modelo, año, etc.)
     Coche coche1("Toyota", "Corolla", 2023, "Automatica", 22000.0, 4, true);
     Coche coche2("Ford", "Explorer", 2022, "Automatica", 35000.0, 5, true);
     Coche coche3("Chevrolet", "Camaro", 2021, "Manual", 40000.0, 2, true);
     Coche coche4("Tesla", "Model 3", 2023, "Automatica", 45000.0, 4, true);
-    Coche coche5("Nissan", "Versa", 2020, "Manual", 16000.0, 4, false);
-    Coche coche6("RAM", "1500", 2023, "Automatica", 42000.0, 4, true);
 
     // Se crean objetos de la clase Moto, con diferentes tipos
     Moto moto1("Yamaha", "YZF-R6", 2021, "Manual", 12000.0, "Deportiva");
@@ -133,12 +152,10 @@ int main(){
     // Se agregan vehículos a las concesionarias
     consecionaria1.agregarVehiculo(coche1); 
     consecionaria1.agregarVehiculo(coche2);  
-    consecionaria1.agregarVehiculo(coche3);
+    consecionaria2.agregarVehiculo(coche3);
     consecionaria1.agregarVehiculo(moto1);
     consecionaria1.agregarVehiculo(moto2);
     consecionaria2.agregarVehiculo(coche4);
-    consecionaria2.agregarVehiculo(coche5);
-    consecionaria2.agregarVehiculo(coche6);
     consecionaria2.agregarVehiculo(moto3);
     consecionaria2.agregarVehiculo(moto4);
     
@@ -151,57 +168,66 @@ int main(){
 
     // Solicitar al usuario que ingrese sus datos
     cout << "Ingrese su nombre: "; 
-    cin >> nombre;
+    getline(cin, nombre);
     cout << "Ingrese su numero de telefono: "; 
-    cin >> numero;
+    getline(cin, numero);
     cout << "Ingrese su correo: "; 
-    cin >> correo;
+    getline(cin, correo);
 
+    limpiar_consola();
     // Crear un objeto Cliente con los datos ingresados
     Cliente cliente1(nombre, correo, numero, consecionaria);
 
     // Variable para almacenar la opción seleccionada por el usuario
     int opcion;
     do {
-
+        
         // Muestra el menú principal de opciones
         mostrarMenu();
 
         // Lee la opción seleccionada por el usuario
         cin >> opcion;
-
+        
         // Estructura de control para realizar acciones dependiendo de la opción
         switch(opcion){
             case 1:
+                // Mostrar información de la concesionaria 1
                 cliente1.set_consecionaria(consecionaria1.get_ubicacion());
                 cout << "Concesionaria seleccionada: " << cliente1.get_concesionaria() << endl;
                 cout << consecionaria1.informacion() << endl;
                 break;
 
             case 2:
+                // Mostrar información de la concesionaria 2
                 cliente1.set_consecionaria(consecionaria2.get_ubicacion());
                 cout << "Concesionaria seleccionada: " << cliente1.get_concesionaria() << endl;
                 cout << consecionaria2.informacion() << endl;
                 break; 
 
             case 3: 
+                limpiar_consola();
                 int Nconsecionaria;
                 mostrarMenuC(); // Muestra las concesionarias disponibles
                 cin >> Nconsecionaria;
 
+                // Agregar un vehículo a la concesionaria seleccionada
                 if (Nconsecionaria == 1) {
-                    agregarNuevoVehiculo(consecionaria1); // Agrega vehículo a la concesionaria 1
+                    limpiar_consola();
+                    agregarNuevoVehiculo(consecionaria1); // Agregar vehículo a la concesionaria 1
                 } else if (Nconsecionaria == 2) {
-                    agregarNuevoVehiculo(consecionaria2); // Agrega vehículo a la concesionaria 2
+                    limpiar_consola();
+                    agregarNuevoVehiculo(consecionaria2); // Agregar vehículo a la concesionaria 2
                 }
                 break;
 
             case 4:
+                limpiar_consola();
                 cout << "Saliendo del programa..." << endl;
                 break;
 
             default:
-                cout << "Opción no válida. Intente nuevamente." << endl;
+                limpiar_consola();
+                cout << "Opción no válida. Intente nuevamente." << endl;  // Mensaje para una opción inválida
         }
 
     } while (opcion != 4);  // El ciclo continua hasta que el usuario elige salir
